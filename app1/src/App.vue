a828f53f15072c6a432ffc5e2b1f47019f0cf87d
Vladislav <ott.vladis@yandex.ru>
2024-11-06 21:10:29 +0700

    Initial commit

---
+++ app1/src/App.vue
@@ -0,0 +1,79 @@
+<script>
+export default {
+  data() {
+    return {
+      city: "",
+      error: ""
+    }
+  },
+  methods: {
+    getWeather(){
+      if(this.city.trim().length < 2) {
+        this.error = "Введите более одного символа"
+        return false
+      }
+
+    }
+  }
+};
+</script>
+
+<template>
+  <div class="container">
+    <h1>Погодное приложение</h1>
+    <p>Узнать погоду в {{ city== "" ? "вашем городе" : city }}</p>
+    <input type="text"  v-model="city" placeholder="Введите город">
+    <button v-show="city !=''" @click="getWeather()">Узнать</button>
+    <p v-if="error">{{ error }}</p>
+  </div>
+</template>
+
+<style scoped>
+.container {
+  width: 900px;
+  height: 500px;
+  border-radius: 50px;
+  padding: 20px;
+  background: #1f0f24;
+  text-align: center;
+  color: white;
+}
+
+.container h1 {
+  margin-top: 50px;
+}
+
+.container p {
+  margin-top: 20px;
+}
+
+.container input {
+  margin-top: 30px;
+  background: transparent;
+  border: 0;
+  border-bottom: 2px solid #110813;
+  color: #fcfcfc;
+  font-size: 14px;
+  padding: 5px 8px;
+  outline: none;
+}
+
+.container input:focus {
+  border-bottom-color: #6e2d7d ;
+}
+
+.container button {
+  background: #e3bc4b;
+  color: #fff;
+  border-radius: 10px;
+  border: 2px solid #b99935;
+  padding: 10px 15px;
+  margin-left: 20px;
+  cursor: pointer;
+  transition: transform 500ms ease;
+}
+
+.container button:hover {
+  transform: scale(1.1) translateY(-5px)
+}
+</style>
---
+++ app1/src/components/TheWelcome.vue
@@ -0,0 +1,19 @@
+<script>
+import WelcomeItem from './WelcomeItem.vue';
+
+export default {
+  name: "parent",
+  component: {WelcomeItem,},
+
+  
+};
+
+</script>
+
+<template>
+ <div class="parent">
+  <WelcomeItem>
+    <p>Hi</p>
+  </WelcomeItem>
+ </div>
+</template>
